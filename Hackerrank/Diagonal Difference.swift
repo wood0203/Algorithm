cef8c71429ab949b127a79d9c29cae81ae7547a4
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-02-22 13:35:35 +0900

    Solve
    순수함수를 이용할 수 없을까

---
+++ Hackerrank/Diagonal Difference.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+func diagonalDifference(arr: [[Int]]) -> Int {
+    var leftToRight = 0
+    var rightToLeft = 0
+    arr.enumerated().forEach {
+        leftToRight += $1[$0]
+        rightToLeft += $1[arr.count-1-$0]
+    }
+    
+    return abs(leftToRight - rightToLeft)
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+var arr = [[Int]]()
+
+for _ in 1...n {
+    guard let arrRowTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+    let arrRow: [Int] = arrRowTemp.split(separator: " ").map {
+        if let arrItem = Int($0) {
+            return arrItem
+        } else { fatalError("Bad input") }
+    }
+
+    guard arrRow.count == n else { fatalError("Bad input") }
+
+    arr.append(arrRow)
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = diagonalDifference(arr: arr)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
